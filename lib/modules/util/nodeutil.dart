d8970af5633a9c62456177005147be0346076dca
alvinow <alvinowijaksana@gmail.com>
2025-01-23 12:43:26 +0700

    #311 commit

--- lib/modules/util/nodeutil.dart
+++ lib/modules/util/nodeutil.dart
@@ -117,56 +117,7 @@ class NodeUtil {
     return returnValue;
   }
 
-  static VwFieldValue? getFieldValueByLocalFieldRef(
-      {required VwRowData rowData, required VwLocalFieldRef localFieldRef}) {
-    VwFieldValue? returnValue;
-
-    try {
-      VwFieldValue? tempFieldValue;
-      if (localFieldRef.localFieldName != null) {
-        tempFieldValue = rowData.getFieldByName(localFieldRef.localFieldName);
-
-        if (localFieldRef.internalFieldName != null) {
-          if (tempFieldValue != null &&
-              tempFieldValue.valueTypeId == VwFieldValue.vatValueLinkNode &&
-              tempFieldValue!.valueLinkNode != null) {
-
-            tempFieldValue = NodeUtil.extractRowDataFromLinkNode(
-                    tempFieldValue!.valueLinkNode!)!
-                .getFieldByName(localFieldRef.internalFieldName!);
-
-            if (localFieldRef.internalSubFieldName != null) {
-              tempFieldValue = NodeUtil.extractRowDataFromLinkNode(
-                      tempFieldValue!.valueLinkNode!)!
-                  .getFieldByName(localFieldRef.internalSubFieldName!);
-
-              if (localFieldRef.internalSub2FieldName != null) {
-                tempFieldValue = NodeUtil.extractRowDataFromLinkNode(
-                        tempFieldValue!.valueLinkNode!)!
-                    .getFieldByName(localFieldRef.internalSub2FieldName!);
-
-                if (localFieldRef.internalSub3FieldName != null) {
-                  tempFieldValue = NodeUtil.extractRowDataFromLinkNode(
-                          tempFieldValue!.valueLinkNode!)!
-                      .getFieldByName(localFieldRef.internalSub3FieldName!);
-                  returnValue = tempFieldValue;
-                } else {
-                  returnValue = tempFieldValue;
-                }
-              } else {
-                returnValue = tempFieldValue;
-              }
-            } else {
-              returnValue = tempFieldValue;
-            }
-          }
-        } else {
-          returnValue = tempFieldValue;
-        }
-      }
-    } catch (error) {}
-    return returnValue;
-  }
+  
 
   static VwFieldValue? getFieldValueFromLinkNodeRowData(
       {required String fieldName, required VwLinkNode linkNode}) {
